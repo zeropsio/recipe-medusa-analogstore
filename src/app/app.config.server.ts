84964974ab9c0713381015d5467b8a068350ceba
Ales Rechtorik <foxxcz@gmail.com>
2025-02-06 20:08:08 +0100

    remove unnecesary providers

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -3,16 +3,10 @@ import { provideServerRendering } from '@angular/platform-server';
 
 import { appConfig } from './app.config';
 import { provideMedusaConfig } from './services/medusa.service';
-import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
-import { requestContextInterceptor } from '@analogjs/router';
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
-    provideHttpClient(
-      withFetch(),
-      withInterceptors([requestContextInterceptor])
-    ),
     provideMedusaConfig({
       baseUrl: import.meta.env['VITE_MEDUSA_BACKEND_URL'] || '',
       publishableKey: import.meta.env['VITE_MEDUSA_CHANNEL_PUBLISHABLE_KEY'],
