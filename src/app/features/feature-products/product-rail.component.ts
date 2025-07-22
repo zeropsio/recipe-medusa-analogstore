491530a046f97882f3ce0c55fa700ed39ff8a47c
robingotz <tug29225@temple.edu>
2025-07-23 01:22:13 +0200

    fix: getting rid of duplicate backend calls
    up until now we had api calls instead of hydration on page load
    by moving to resource and upgrading dependencies we now have
    the page load and display our featured products even if javascript
    is disabled

--- src/app/features/feature-products/product-rail.component.ts
+++ src/app/features/feature-products/product-rail.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, input, OnInit, signal } from '@angular/core';
+import {Component, inject, input, resource} from '@angular/core';
 import { HttpTypes } from '@medusajs/types';
 import { ProductPreviewComponent } from '../products/product-preview.component';
 import { MedusaService } from 'src/app/services/medusa.service';
@@ -7,34 +7,31 @@ import { MedusaService } from 'src/app/services/medusa.service';
   selector: 'product-rail',
   standalone: true,
   imports: [ProductPreviewComponent],
-  template: ` <div class="container py-12 sm:py-24">
-    <div class="flex justify-between mb-8">
-      <h2 class="txt-xlarge">{{ collection()?.title }}</h2>
-      <a href="">View all</a>
-    </div>
-    <ul
-      class="grid grid-cols-2 small:grid-cols-3 gap-x-6 gap-y-24 small:gap-y-36"
-    >
-      @for(product of products(); track product?.['id']){
-      <li>
-        <product-preview [product]="product" />
-      </li>
-      }
-    </ul>
-  </div>`,
+  template: `
+    <div class="container py-12 sm:py-24">
+      <div class="flex justify-between mb-8">
+        <h2 class="txt-xlarge">{{ collection()?.title }}</h2>
+        <a href="">View all</a>
+      </div>
+      <ul
+        class="grid grid-cols-2 small:grid-cols-3 gap-x-6 gap-y-24 small:gap-y-36"
+      >
+        @for (product of products.value()?.response?.products ?? []; track product?.['id']) {
+          <li>
+            <product-preview [product]="product"/>
+          </li>
+        }
+      </ul>
+    </div>`,
 })
-export class ProductRailComponent implements OnInit {
+export class ProductRailComponent {
   private _medusa = inject(MedusaService);
-  protected products = signal<HttpTypes.StoreProduct[]>([]);
+
+  protected products = resource({
+    request: () => ({regionId: this.region()?.id || 'US',}),
+    loader: (params) => this._medusa.productList(params.request)
+  });
 
   public collection = input<HttpTypes.StoreCollection | undefined>();
   public region = input.required<HttpTypes.StoreRegion | null | undefined>();
-
-  ngOnInit(): void {
-    this._medusa
-      .productList({
-        regionId: this.region()?.id || 'US',
-      })
-      .then(({ response: { products } }) => this.products.set(products));
-  }
 }
